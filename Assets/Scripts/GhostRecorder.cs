9f40195ad41ed03f96521a8607142e426a772cdc
JFergus98 <55389749+JFergus98@users.noreply.github.com>
2023-03-13 18:21:30 +0000

    Update GhostRecorder.cs

--- Assets/Scripts/GhostRecorder.cs
+++ Assets/Scripts/GhostRecorder.cs
@@ -5,44 +5,54 @@ using UnityEngine;
 public class GhostRecorder : MonoBehaviour
 {
     [SerializeField]
-    private GhostScriptableObject ghost;
+    private GhostScriptableObject ghostScrObj;
     private int index;
     private float timer;
     private float time;
-
     private int maxRecordings = 11;
 
-    // Start is called before the first frame update
-    void Awake()
+    // Awake is called when the script instance is being loaded
+    private void Awake()
     {
-        if (ghost.isRecord) {
-            
-            ghost.ghostDataList.Insert(0, new GhostDataList());
-            if (ghost.ghostDataList.Count > maxRecordings) {
-                ghost.ghostDataList.RemoveRange(maxRecordings, ghost.ghostDataList.Count-maxRecordings);
-            }
+        if (ghostScrObj.isRecord) {
+            // Add new ghost to the start of the list
+            ghostScrObj.ghosts.Insert(0, new Ghost());
 
+            // If List size is greater than max recordings, then remove excess items at end of list
+            if (ghostScrObj.ghosts.Count > maxRecordings) {
+                ghostScrObj.ghosts.RemoveRange(maxRecordings, ghostScrObj.ghosts.Count-maxRecordings);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if (ghostScrObj.isRecord) {
+            // Set time and timer to 0
             timer = 0;
             time = 0;
         }
     }
 
-    void LateUpdate()
+    // LateUpdate is called after all Update functions have been called
+    private void LateUpdate()
     {
         // Start timer and increment time
         timer += Time.deltaTime;
         time += Time.deltaTime;
 
         // If recording and it is time to record
-        if (ghost.isRecord && timer >= 1/ghost.recordFrequency) {
+        if (ghostScrObj.isRecord && timer >= 1/ghostScrObj.recordFrequency) {
 
             // Get current time, position and rotation data
             GhostData data = new GhostData(time, this.transform.position, this.transform.rotation);
             
-            // Add data to the list
-            ghost.ghostDataList[0].ghostData.Add(data);
+            // Add data to the new first ghost in the list
+            ghostScrObj.ghosts[0].ghostData.Add(data);
 
-            //Debug.Log("Recorded: " + data.position);
+            //Debug.Log("Recorded: " + data); // Testing
+
             // Reset timer
             timer = 0;
         }
