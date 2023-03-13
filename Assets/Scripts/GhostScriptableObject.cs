5aaafbcbfd2f675ec9adbeb43c4bade29630a82a
JFergus98 <55389749+JFergus98@users.noreply.github.com>
2023-03-13 15:57:25 +0000

    Create GhostScriptableObject.cs

---
+++ Assets/Scripts/GhostScriptableObject.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GhostScriptableObject", menuName = "ScriptableObjects/Ghost")]
+public class GhostScriptableObject : ScriptableObject
+{
+    [field: SerializeField]
+    public bool isRecord { get; private set; }
+    [field: SerializeField]
+    public bool isReplay { get; private set; }
+    [field: SerializeField]
+    public float recordFrequency { get; private set; }
+
+    [field: SerializeField]
+    public List<GhostDataList> ghostDataList { get; private set; }
+}
