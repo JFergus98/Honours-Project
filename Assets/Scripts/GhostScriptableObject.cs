f211e62bae444e400251c69aa5dde40f0cbd3e38
JFergus98 <55389749+JFergus98@users.noreply.github.com>
2023-03-20 06:45:43 +0000

    Update GhostScriptableObject.cs

--- Assets/Scripts/GhostScriptableObject.cs
+++ Assets/Scripts/GhostScriptableObject.cs
@@ -7,10 +7,6 @@ using UnityEngine;
 public class GhostScriptableObject : ScriptableObject
 {
     [field: SerializeField]
-    public bool isRecord { get; private set; }
-    [field: SerializeField]
-    public bool isReplay { get; private set; }
-    [field: SerializeField]
     public float recordFrequency { get; private set; }
 
     [field: SerializeField]
