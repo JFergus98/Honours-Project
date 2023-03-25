c52892524b0ba9b09fc6da56c440804f29e1e036
JFergus98 <55389749+JFergus98@users.noreply.github.com>
2023-03-25 09:17:18 +0000

    Create LoopCounter.cs

---
+++ Assets/Scripts/LoopCounter.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class LoopCounter : MonoBehaviour
+{
+    private static LoopCounter _instance;
+
+    public static LoopCounter Instance {
+        get {
+            return _instance;
+        }
+    }
+    private int loopCount;
+
+    // Awake is called when the script instance is being loaded
+    private void Awake()
+    {
+        // If there is already an instance, then delete self;
+        if (_instance != null && _instance != this) {
+            Debug.Log("An instance of LoopCounter already exists in the scene.");
+            Destroy(this.gameObject);
+        }
+        else {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        loopCount++;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
