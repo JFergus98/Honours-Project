c1ea67876612dcc791a286d9e79d7c973fc552ef
JFergus98 <55389749+JFergus98@users.noreply.github.com>
2023-04-12 22:53:17 +0100

    Update SpawnFakePlatforms.cs

--- Assets/Scripts/SpawnFakePlatforms.cs
+++ Assets/Scripts/SpawnFakePlatforms.cs
@@ -6,17 +6,10 @@ public class SpawnFakePlatforms : MonoBehaviour
 {
     [SerializeField]
     private SeedScriptableObject seedScrObj;
-
     [SerializeField]
     private int NoOfRows;
     [SerializeField]
     private List<PlatformRow> platformList = new List<PlatformRow>();
-
-    // Awake is called when the script instance is being loaded
-    private void Awake()
-    {
-        
-    }
 
     // Start is called before the first frame update
     private void Start()
@@ -25,24 +18,11 @@ public class SpawnFakePlatforms : MonoBehaviour
         //Random.InitState(5);
         Random.InitState(seedScrObj.seed);
 
-        // int Col = platformList.Count / NoOfRows;
-
-        // for (int i = 0; i < platformList.Count; i += Col) {
-        //     int index = Random.Range(i, i + Col);
-        //     platformList[index].GetComponent<Collider>().enabled = true;
-        //     Debug.Log(index);
-        // }
-
-        foreach (PlatformRow row in platformList) {
+        foreach (PlatformRow row in platformList)
+        {
             int index = Random.Range(0, row.count());
-                row.getElementAt(index).GetComponent<Collider>().enabled = true;
-                Debug.Log("fake: " + index);
+            row.getElementAt(index).GetComponent<Collider>().enabled = true;
+            Debug.Log("fake: " + index);
         }
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-        
     }
 }
